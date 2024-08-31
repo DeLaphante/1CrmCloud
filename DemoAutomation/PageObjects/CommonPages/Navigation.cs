2eba78465470ec6f19dec3bd2e1798f6e044c19b
Question Everything <boygadget@hotmail.com>
2024-08-31 21:52:50 +0100

    fix

--- DemoAutomation/PageObjects/CommonPages/Navigation.cs
+++ DemoAutomation/PageObjects/CommonPages/Navigation.cs
@@ -37,7 +37,7 @@ namespace DemoAutomation.PageObjects.CommonPages
 
         public void NavigateToMenuOption(string option)
         {
-            if (CloseMessageDialog_button.IsDisplayed() & CloseMessageDialog_button.IsDisplayed())
+            if (CloseMessageDialog_button.IsDisplayed() | CloseMessageDialog_button.IsDisplayed())
                 CloseMessageDialog_button.Click();
 
             switch (option.ToLower())
