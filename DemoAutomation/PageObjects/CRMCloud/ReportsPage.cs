8600fc1a027d422115fd908f9cb1077696e89672
Question Everything <boygadget@hotmail.com>
2026-06-04 17:28:31 +0100

    update test

--- DemoAutomation/PageObjects/CRMCloud/ReportsPage.cs
+++ DemoAutomation/PageObjects/CRMCloud/ReportsPage.cs
@@ -15,6 +15,7 @@ namespace DemoAutomation.PageObjects.CommonPages
         #region Locators
 
         PageElement SearchFilter_textbox => new PageElement(_Driver, By.Id("filter_text"));
+        PageElement Results_label(string report) => new PageElement(_Driver, By.XPath($"//div[contains(@class,'input-label') and text()='{report}']"));
         PageElement Results_link(string report) => new PageElement(_Driver, By.XPath($"//a[text()='{report}']"));
         PageElement Button_button(string text, int index = 1) => new PageElement(_Driver, By.XPath($"(//*[(self::button or self::a or self::input or @onclick or @role='button')  and (contains(translate(normalize-space(string(.)),'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz'),\"{text.ToLower()}\") or contains(@class,\"{text}\") or contains(@title,\"{text}\") or contains(@value,\"{text}\")) and not(contains(@class,'disable') or @disabled)])[{index}]"));
         PageElement ResultsRows_label => new PageElement(_Driver, By.XPath($"//tbody/tr[contains(@class,'listViewRow')]"));
@@ -25,9 +26,8 @@ namespace DemoAutomation.PageObjects.CommonPages
 
         public void RunReport(string report)
         {
-            SearchFilter_textbox.Click();
-            SearchFilter_textbox.SendKeys(report + Keys.Enter);
-            Results_link(report).Click();
+            SearchFilter_textbox.SendKeys(report);
+            Results_label(report).Click();
             Button_button("Run Report").Click();
         }
 
